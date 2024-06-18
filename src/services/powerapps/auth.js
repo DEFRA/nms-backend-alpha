d5e598b98e7144da01e11f15780d40cafb2c016e
Fujale Ahmed Shaik <shaikfujale.ahmed@cognizant.com>
2024-06-18 14:31:15 +0100

    392804 - Test commit

--- src/services/powerapps/auth.js
+++ src/services/powerapps/auth.js
@@ -5,7 +5,6 @@ const tenantId = config.get('azTenantId')
 const clientId = config.get('azClientId')
 const clientSecret = config.get('azClientSecret')
 const resourceUrl = config.get('dataverseUri')
-console.log(`${tenantId}-${clientId}-${clientSecret}-${resourceUrl}`)
 
 const azConfig = {
   auth: {
