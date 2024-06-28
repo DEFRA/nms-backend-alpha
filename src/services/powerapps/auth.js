d445ce04e651b55fff668442e0f0a48b829b0ce4
Fujale Ahmed Shaik <shaikfujale.ahmed@cognizant.com>
2024-06-28 17:06:51 +0100

    Using the proxyFetch while getting the azure token

--- src/services/powerapps/auth.js
+++ src/services/powerapps/auth.js
@@ -1,12 +1,11 @@
 import { ConfidentialClientApplication } from '@azure/msal-node'
-import { proxyAgent } from '~/src/helpers/proxy-agent'
 import { config } from '~/src/config'
+import { proxyFetch } from '~/src/helpers/proxy-fetch'
 
 const tenantId = config.get('azTenantId')
 const clientId = config.get('azClientId')
 const clientSecret = config.get('azClientSecret')
 const resourceUrl = config.get('dataverseUri')
-const proxyAgentObj = proxyAgent()
 
 const azConfig = {
   auth: {
@@ -14,23 +13,16 @@ const azConfig = {
     authority: `https://login.microsoftonline.com/${tenantId}`,
     clientSecret
   },
-  ...(proxyAgentObj && {
-    system: {
-      networkClient: {
-        sendGetRequestAsync: async (url, options) => {
-          return await fetch(url, { ...options, agent: proxyAgentObj.agent })
-        },
-        sendPostRequestAsync: async (url, options) => {
-          const sendingOptions = options || {}
-          sendingOptions.method = 'post'
-          return await fetch(url, {
-            ...sendingOptions,
-            agent: proxyAgentObj.agent
-          })
-        }
+  system: {
+    networkClient: {
+      sendGetRequestAsync: async (url, options) => {
+        return proxyFetch(url, options)
+      },
+      sendPostRequestAsync: async (url, options) => {
+        return proxyFetch(url, { ...options, method: 'POST' })
       }
     }
-  })
+  }
 }
 
 const client = new ConfidentialClientApplication(azConfig)
