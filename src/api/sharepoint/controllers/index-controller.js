49035b0e8d33c3bb19c1e1c7f40ce1f7fd3c30bb
kaushikmaitracts <kaushik.maitra@cognizant.com>
2024-11-01 12:30:04 +0000

    added code for uploading to SP

--- src/api/sharepoint/controllers/index-controller.js
+++ src/api/sharepoint/controllers/index-controller.js
@@ -1,15 +1,15 @@
-// import { GetObjectCommand } from '@aws-sdk/client-s3'
+import { GetObjectCommand } from '@aws-sdk/client-s3'
 import { ObjectId } from 'mongodb'
 
-// import { config } from '~/src/config/index.js'
+import { config } from '~/src/config/index.js'
 import { mongoCollections } from '~/src/helpers/constants'
 import { readDocument } from '~/src/helpers/database-transaction'
-// import { s3Client } from '~/src/helpers/s3-client.js'
-// import { uploadToSharePoint } from '~/src/services/powerapps/dataverse'
-// import { streamToBuffer } from '../helpers/stream-to-buffer'
-import { callLogicApp } from '~/src/services/powerapps/dataverse'
+import { s3Client } from '~/src/helpers/s3-client.js'
+import { uploadToSharePoint } from '~/src/services/powerapps/dataverse'
+import { streamToBuffer } from '../helpers/stream-to-buffer'
+// import { callLogicApp } from '~/src/services/powerapps/dataverse'
 
-/* const indexController = {
+const indexController = {
   handler: async (request, h) => {
     request.logger.info('Reading Document from MongoDB')
     const { id, collection } = request.params
@@ -33,35 +33,32 @@ import { callLogicApp } from '~/src/services/powerapps/dataverse'
 
           const response = await s3Client.send(command)
           const fileBuffer = await streamToBuffer(response.Body)
-          request.logger.info('fileBuffer >>>    ' + fileBuffer)
           const folderUrl =
             '/sites/NutrientNeutralityprojectdelivery/Credit Sales/Avon/NM-D-Av-0008/Applications'
           const uploadUrl = `https://defradev.sharepoint.com${folderUrl}/_api/web/getfolderbyserverrelativeurl('${folderUrl}')/files/add(overwrite=true, url='${filename}')`
-          const logicAppUrl = 'https://devnmswebaf1401.azurewebsites.net:443/api/testworkflow1/triggers/When_a_HTTP_request_is_received/invoke?api-version=2022-05-01&sp=%2Ftriggers%2FWhen_a_HTTP_request_is_received%2Frun&sv=1.0&sig=trstytkwONJ7yjp7Dd4ABqxKQmBdwbRDRX2iYtuHeM0'
-          request.logger.info('logicAppUrl >> ' + logicAppUrl)
+          // const logicAppUrl = 'https://devnmswebaf1401.azurewebsites.net:443/api/testworkflow1/triggers/When_a_HTTP_request_is_received/invoke?api-version=2022-05-01&sp=%2Ftriggers%2FWhen_a_HTTP_request_is_received%2Frun&sv=1.0&sig=trstytkwONJ7yjp7Dd4ABqxKQmBdwbRDRX2iYtuHeM0'
           // return h.response('Successful without Uploading to SP').code(200)
-          const logicAppResponse = await callLogicApp(logicAppUrl)
-          request.logger.info('logicAppResponse >> ' + logicAppResponse)
+          request.logger.info('fileBuffer before uploading    ' + fileBuffer)
           const uploadResponse = await uploadToSharePoint(uploadUrl, fileBuffer)
           request.logger.info('uploadResponse >> ' + uploadResponse)
           // .header('Content-Type', response.ContentType)
-          return h.response(logicAppResponse).code(200)
+          return h.response(uploadResponse).code(200)
         } else {
           return h
-            .response({ document, error: 'Document does not have file' })
+            .response({ document, error: 'Document does not have file in S3' })
             .code(404)
         }
       } else {
-        return h.response({ error: 'Document not found' }).code(404)
+        return h.response({ error: 'Document not found in MongoDB' }).code(404)
       }
     } catch (err) {
       request.logger.error(err)
-      return h.response('File Not Found').code(404)
+      return h.response('Error Uploading file to SharePoint').code(404)
     }
   }
-} */
+}
 
-const indexController = {
+/* const indexController = {
   handler: async (request, h) => {
     request.logger.info('Reading Document from MongoDB')
     const { id, collection } = request.params
@@ -98,6 +95,6 @@ const indexController = {
       return h.response('LogicApp Error').code(404)
     }
   }
-}
+} */
 
 export { indexController }
